f240f49508b320ad36e1500ed29aca71bca90052
jaeyoung-jin <87025696+jaeyoung-jin@users.noreply.github.com>
2022-12-01 21:45:40 +0900

    2022-12-01

--- 4673.py
+++ 4673.py
@@ -1,12 +1,18 @@
+# 셀프 넘버
+
+# 생성자 n을 통해 생성자가 있는 수(셀프넘버가 아닌 수)를 구하는 함수를 정의한다.
 def d(n: int) -> int:
     n = n + sum(map(int,str(n)))
     return n
 
+# 생성자가 있는 수(셀프 넘버가 아닌 수)를 넣을 집합
 non_selfnum = set()
 
+# 1부터 10000까지의 수를 이용해 생성자가 있는 수(셀프 넘버가 아닌 수)를 구한다.
 for i in range(1, 10001):
     non_selfnum.add(d(i))
 
+# 셀프 넘버를 출력한다.
 for i in range(1, 10001):
     if i not in non_selfnum:
         print(i)
